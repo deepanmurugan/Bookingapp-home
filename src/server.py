96b4e92017be09776143bc1f660e2b6a279bf4f7
Deepan M <55130173+deepanmurugan@users.noreply.github.com>
2020-12-19 14:45:37 +0530

    Create server.py

---
+++ src/server.py
@@ -0,0 +1,29 @@
+import time
+import redis
+import socket
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis-lb', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hit():
+    count = get_hit_count()
+    return "Welcome to BookingApp - Home Page - on node %s. This page has been hit %i times since deployment" % ( socket.gethostname(), int(count))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
