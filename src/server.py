50cff6973a2ca14b844379d9127a6372f22605e0
Deepan M <55130173+deepanmurugan@users.noreply.github.com>
2020-12-19 22:10:57 +0530

    Update server.py

--- src/server.py
+++ src/server.py
@@ -4,7 +4,7 @@ import socket
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis-lb', port=6379)
+cache = redis.Redis(host='redis.bookingapp.local.com', port=6379)
 
 
 def get_hit_count():
